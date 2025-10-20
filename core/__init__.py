0970161016e8b4c316d3620233e1a16a3a86071c
vtmathias03 <dm.mathias@yahoo.com.br>
2025-10-20 10:17:08 -0300

    Delete core directory

--- core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-RAG System Core Modules
-"""
-
-from .advanced_chunker import AdvancedChunker, HierarchicalChunk
-from .chromadb_manager import ChromaDBManager
-
-__all__ = ['AdvancedChunker', 'HierarchicalChunk', 'ChromaDBManager']
